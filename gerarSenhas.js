38211ce0c095f7054ca82793928763974731e690
Kamila Cavalcante <141283305+kamimarinn@users.noreply.github.com>
2024-02-19 10:12:46 -0300

    Add files via upload

--- gerarSenhas.js
+++ gerarSenhas.js
@@ -1,15 +1,15 @@
-function gerarSenhas() {
+function gerar() {
     var tamanho = parseInt(document.getElementById("tamanho").value);
     var quantidade = parseInt(document.getElementById("quantidade").value);
-    var usarCaracteresEspeciais = document.getElementById("caracteresEspeciais").checked;
+    var usarCaracteresEspeciais = document.getElementById("especial").checked;
     var usarNumeros = document.getElementById("numeros").checked;
-    var usarMaiusculas = document.getElementById("maiusculas").checked;
-    
-    var caracteres = "";
+    var usarMaiusculas = document.getElementById("letra").checked;
+ 
+    var caracteres = "abcdefghijklmnopqrstuvwxyz";
     if (usarCaracteresEspeciais) caracteres += "!@#$%&*()+=-*./";
     if (usarNumeros) caracteres += "0123456789";
     if (usarMaiusculas) caracteres += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
+ 
     var senhas = [];
     for (var i = 0; i < quantidade; i++) {
         var senha = "";
@@ -19,11 +19,15 @@ function gerarSenhas() {
         }
         senhas.push(senha);
     }
-    
+ 
     document.getElementById("senhasGeradas").value = senhas.join("\n");
 }
  
-function limparInputs() {
-    document.getElementById("senhaForm").reset();
+function limpar() {
+    document.getElementById("tamanho").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("especial").checked = false;
+    document.getElementById("numeros").checked = false;
+    document.getElementById("letra").checked = false;
     document.getElementById("senhasGeradas").value = "";
 }
